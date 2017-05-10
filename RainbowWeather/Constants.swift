ddfe38a60b94d9981a4700cd08b609cd7e413943
Mbusi Hlatshwayo <mbusi@cox.net>
2017-05-10 11:27:33 -0700

    added problem file again

---
+++ RainbowWeather/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  constants.swift
+//  RainbowWeather
+//
+//  Created by Mbusi Hlatshwayo on 5/9/17.
+//  Copyright © 2017 RainbowApps. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let MY_API_KEY = "615615de6ee7edd99b2c5e25110fc424"
+let WEATHER_URL = "\(BASE_URL)\(LATITUDE)-25\(LONGITUDE)134\(APP_ID)\(MY_API_KEY)"
+let FORECAST_WEATHER_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10"
+typealias DownloadComplete = () -> ();
